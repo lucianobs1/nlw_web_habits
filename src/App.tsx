b0b4e9541c150b59291443846707d1d1e890c51a
lucianobs1 <lucianojob1@hotmail.com>
2023-01-17 22:10:27 -0300

    initial configs added

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/global.css';
+
+import { Habit } from './components/Habit';
+
+function App() {
+  return (
+    <>
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+    </>
+  );
+}
+
+export default App;
