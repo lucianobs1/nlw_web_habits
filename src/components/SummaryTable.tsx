f0de19c30759474a0fd6597fb9e6b45282712eee
lucianobs1 <lucianojob1@hotmail.com>
2023-01-24 10:30:41 -0300

    updated component SummaryTable

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,7 +1,7 @@
 import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning';
 import { HabitDay } from './HabitDay';
 
-const weeekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const summaryDates = generateDatesFromYearBeginning();
 
 const minimumSummaryDatesSize = 18 * 7; // 18 weeks
@@ -11,7 +11,7 @@ function SummaryTable() {
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weeekDays.map((weekDay, index) => {
+        {weekDays.map((weekDay, index) => {
           return (
             <div
               key={`${weekDay}-${index}`}
@@ -25,7 +25,13 @@ function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />;
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          );
         })}
 
         {amountOfDaysToFill > 0 &&
